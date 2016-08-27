47dc0f777aad9c0ec6d2edfb340fdb6799ed8dc0
Shane Wilson <shane.wilson@oicr.on.ca>
2016-08-27 17:45:58 -0400

    [DCC-1,DCC-2] more changelog

--- bin/changelog.js
+++ bin/changelog.js
@@ -1,13 +1,46 @@
 const execa = require('execa');
-const Listr = require('listr');
-const readPkgUp = require('read-pkg-up');
+// const Listr = require('listr');
+// const readPkgUp = require('read-pkg-up');
 
-execa.stdout('git', ['describe', '--abbrev=0', '--tags']).then(tag => {
-  console.log(tag);
-  return execa.stdout('git', ['log', '--pretty=format:%s (%h)', `HEAD...${tag}`]).then(lines => {
-    const log = lines.split('\n').map(l => {
-      return l.replace(/(\[.+\])(.+)(\(.+\))/, "[<a href=''>$1</a>] $2 (<a href=''>$3</a>)");
+const formatLines = (line, matchedPr) => {
+  const ls = [];
+  const tickets = line.tickets.split(',').map(t => `<a href="${process.env.JIRA_URL}/${t}">${t}</a>`);
+  ls.push(`[${tickets}] ${line.title}`);
+  ls.push(`(<a href="https://github.com/${process.env.GIT_REPO}/commit/${line.commit}">${line.short}</a>)`);
+  if (matchedPr) {
+    ls.push(`<a href="${matchedPr.url}">#${matchedPr.number}</a>`);
+  }
+  return ls.join(' ');
+};
+
+execa.stdout('git', ['describe', '--abbrev=0', '--tags']).then(tag => (
+  execa.stdout('git', ['log', '--pretty=format:%s__SPLIT__%H__SPLIT__%h)', `HEAD...${tag}`]).then(ls => {
+    const lines = ls.split('\n');
+    execa.stdout('curl', [`https://api.github.com/repos/${process.env.GIT_REPO}/pulls?sort=updated&direction=desc&state=closed`]).then(prs => {
+      const mergedPrs = JSON.parse(prs).filter(pr => pr.merged_at).map(p => ({
+        url: p.url,
+        number: p.number,
+        mergeCommitSha: p.merge_commit_sha,
+      }));
+
+      const log = lines.reduce((acc, l) => {
+        const [tickettitle, commit, short] = l.split('__SPLIT__');
+        const match = tickettitle.match(/^\[(.*)\] (.*)$/);
+        if (!match) return acc;
+
+        const [_, tickets, title] = match;
+        const line = {
+          title,
+          tickets,
+          commit,
+          short,
+        };
+
+        const matchedPr = mergedPrs.find(pr => pr.mergeCommitSha === line.commit);
+        return [...acc, formatLines(line, matchedPr)];
+      }, []);
+
+      console.log(log);
     });
-    console.log(log);
-  });
-});
+  })
+));
