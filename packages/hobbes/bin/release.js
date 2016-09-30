298168b3a093a3da09c24075cfbd847b18299a2d
Shane Wilson <shanewilson@users.noreply.github.com>
2016-09-30 14:51:08 -0400

    :package: stuff (#3)

--- packages/hobbes/bin/release.js
+++ packages/hobbes/bin/release.js
@@ -6,14 +6,14 @@ const readPkgUp = require('read-pkg-up');
 const pkg = readPkgUp.sync().pkg;
 
 const tasks = new Listr([
-  // {
-  //   title: 'Git Commit Release',
-  //   task: () => execa('git', ['commit', '-a', '-m', `:bookmark: ${process.env.NEXT_VERSION}`]),
-  // },
-  // {
-  //   title: 'Git Tag Release',
-  //   task: () => execa('git', ['tag', process.env.NEXT_VERSION, '-m', process.env.NEXT_VERSION]),
-  // },
+  {
+    title: 'Git Commit Release',
+    task: () => execa('git', ['commit', '-a', '-m', `:bookmark: ${process.env.NEXT_VERSION}`]),
+  },
+  {
+    title: 'Git Tag Release',
+    task: () => execa('git', ['tag', process.env.NEXT_VERSION, '-m', process.env.NEXT_VERSION]),
+  },
   // Publish
   // Push Everything
   // Git Release Notes
