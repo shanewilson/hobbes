d4ba4b7c1bdd881c9c7e8733c919c3ec7ce6e1c5
Shane Wilson <shanezilla@gmail.com>
2016-09-30 14:45:02 -0400

    :sparkles: changelog generator uses squashed gh commits

--- packages/hobbes/bin/changelog.js
+++ packages/hobbes/bin/changelog.js
@@ -3,35 +3,36 @@ const Listr = require('listr');
 const fs = require('graceful-fs');
 const readPkgUp = require('read-pkg-up');
 const dateFormat = require('dateformat');
-const config = require('hobbes-config');
+const config = require('@shanewilson/hobbes-config');
 
 const pkg = readPkgUp.sync().pkg;
 
+const REPO_URL = pkg.repository.replace(/^git\+/, '').replace(/\.git$/, '')
 let FROM_TAG = process.env.FROM_TAG;
 const TO_TAG = process.env.TO_TAG || 'HEAD';
 let LINES = [];
-let PRS = [];
 let LOGS;
 
 const emojiMapping = Object.assign({}, {
   sparkles: 'Features',
   bug: 'Fixes',
-  memo: 'Documentation',
+  books: 'Documentation',
+  racehorse: 'Performance',
 }, (pkg.hobbes || {}).emojiMapping || {});
 
-const formatLines = (line, matchedPr) => {
+const formatLines = (line, pr) => {
   const ls = [];
   ls.push('-');
   if (line.emojis) {
     ls.push(line.emojis);
   }
   if (line.tickets) {
-    const tickets = line.tickets.split(',').map(t => `[${t}](${process.env.JIRA_URL}/${t})`);
+    const tickets = line.tickets.split(',').map(t => `[${t}](${pkg.bugs.url}/${t})`);
     ls.push(`[${tickets}]`);
   }
   ls.push(line.title);
-  ls.push(`([${line.short}](https://github.com/${process.env.GIT_REPO}/commit/${line.commit}))`);
-  if (matchedPr) ls.push(`[#${matchedPr.number}](${matchedPr.url})`);
+  if (pr) ls.push(`([${pr}](${REPO_URL}/pull/${pr}))`);
+  else ls.push(`([${line.short}](${REPO_URL}/commit/${line.commit}))`);
 
   return ls.join(' ');
 };
@@ -62,18 +63,6 @@ const changelog = new Listr([
           },
         ]),
       },
-      {
-        title: 'Fetching Pull Requests',
-        task: () => (
-          execa.stdout('curl', [`https://api.github.com/repos/${process.env.GIT_REPO}/pulls?sort=updated&direction=desc&state=closed`]).then(prs => {
-            PRS = JSON.parse(prs).filter(pr => pr.merged_at).map(p => ({
-              url: p.html_url,
-              number: p.number,
-              mergeCommitSha: p.merge_commit_sha,
-            }));
-          })
-        ),
-      },
     ], { concurrent: true }),
   },
   {
@@ -81,10 +70,10 @@ const changelog = new Listr([
     task: () => {
       const lines = LINES.reduce((acc, l) => {
         const [tickettitle, commit, short] = l.split('__SPLIT__');
-        const match = tickettitle.match(/^(:(.+?):(:.+:)?) (\[(.*)\] )?(.*)$/);
+        const match = tickettitle.match(/^(:(.+?):(:.+:)?) (\[(.*)\] )?(.*)( \(.*\))$/);
         if (!match || !emojiMapping[match[2]]) return acc;
 
-        const [_1, _2, type, emojis, _3, tickets, title] = match;
+        const [_1, _2, type, emojis, _3, tickets, title, _4, pr] = match;
 
         const line = {
           title,
@@ -94,9 +83,8 @@ const changelog = new Listr([
           short,
         };
 
-        const matchedPr = PRS.find(pr => pr.mergeCommitSha === line.commit);
         return Object.assign({}, acc, {
-          [type]: [...(acc[type] || []), formatLines(line, matchedPr)],
+          [type]: [...(acc[type] || []), formatLines(line, pr)],
         });
       }, {});
 
@@ -110,7 +98,7 @@ const changelog = new Listr([
       try {
         const data = fs.readFileSync(CL, 'utf-8');
 
-        const title = `[${process.env.NEXT_VERSION}](https://github.com/${process.env.GIT_REPO}/compare/${FROM_TAG}...${process.env.NEXT_VERSION})`;
+        const title = `[${process.env.NEXT_VERSION}](${REPO_URL}}/compare/${FROM_TAG}...${process.env.NEXT_VERSION})`;
         const nextChangelog = `## ${title} (${dateFormat(new Date(), 'yyyy-mm-dd', true)})\n\n${LOGS}\n\n${data}`;
 
         fs.writeFile(CL, nextChangelog, 'utf-8');
